fd95f75e5b2afe7878752d66b07365ac3d23c0fc
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2019-08-04 16:50:00 +0200

    Add right panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,49 @@ import React from 'react';
 import MapContainer from './map/MapContainer';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-    <div className="header">HEADER</div>
-    <div className="view">
-      <div className="content">
-        <MapContainer />
+class App extends React.Component {
+  openRightPanel = () => {
+    console.log(this.refs.rightPanel);
+    (this.refs.rightPanel as any).classList.toggle('opened');
+  }
+  render() {
+    return (
+      <div className="App">
+      <div className="header">HEADER</div>
+      <div className="view">
+        <div className="content">
+          <MapContainer />
+          <div ref="rightPanel" className="right-panel">
+            <div className="panel">
+            RIGHT PANEL
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            <p>A</p>
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          FOOTER
+          <button onClick={this.openRightPanel}>Open</button>
+        </div>
       </div>
-      <div className="footer">FOOTER</div>
-    </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
